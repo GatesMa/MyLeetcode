b8be310edfbb3229ee58f0b812e97dbdfb12d905
GatesMa <42234931+GatesMa@users.noreply.github.com>
2019-12-23 00:38:23 +0800

    🐲Add sources

---
+++ 6/6_1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows == 1) {
+            return s;
+        }
+        List<StringBuilder> list = new ArrayList<>();
+        for (int i = 0; i < Math.min(numRows, s.length()); i++)
+            list.add(new StringBuilder());
+
+
+        int curRow = 0;
+        boolean goDown = false;
+        for(char c : s.toCharArray()) {
+            list.get(curRow).append(c);
+            if(curRow == 0 || curRow == numRows - 1) goDown = !goDown;
+            curRow += goDown ? 1 : -1;
+        }
+        
+        StringBuilder res = new StringBuilder();
+        for(StringBuilder sb : list) {
+            res.append(sb);
+        }
+        return res.toString();
+    }
+}
