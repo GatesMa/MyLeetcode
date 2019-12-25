ed1132a04f8c815077f908c491228f18550eb583
GatesMa <1353934265@qq.com>
2019-12-25 23:56:18 +0800

    🪐21 合并两个有序链表 - 迭代O(n+m) O(1)

---
+++ 21/21_1.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        ListNode res = new ListNode(0);
+        ListNode node = res;
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                node.next = l1;
+                l1 = l1.next;
+            } else {
+                node.next = l2;
+                l2 = l2.next;
+            }
+            node = node.next;
+        }
+        node.next = l1 == null ? l2 : l1;
+        return res.next;
+    }
+}
