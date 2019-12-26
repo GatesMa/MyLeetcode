845352094d661023dda86938fd862765a5483b66
GatesMa <1353934265@qq.com>
2019-12-26 21:55:13 +0800

    💧23. 合并K个排序链表 - 两两合并变化而来

---
+++ 23/2.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0) return null;
+        ListNode node = new ListNode(0);
+        int len = lists.length;
+        for(int i = 1;i < len;i++) {
+            lists[i] = merge2Lists(lists[i], lists[i - 1]);
+        }
+        return lists[len - 1];
+    }
+
+    public ListNode merge2Lists(ListNode l1, ListNode l2) {
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        if(l1.val < l2.val) {
+            l1.next = merge2Lists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = merge2Lists(l1, l2.next);
+            return l2;
+        }
+    }
+}
