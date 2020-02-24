c2ac75b76eff6cf60da92951f679df5d12e9f872
GatesMa <1353934265@qq.com>
2020-02-24 20:53:21 +0800

    🛡️upd 55 自顶向下 优化

---
+++ 55/3.java
@@ -0,0 +1,33 @@
+enum Index {
+    GOOD, BAD, UNKNOWN
+}
+
+
+class Solution {
+    Index[] memo;
+
+    public boolean canJump(int[] nums) {
+        if(nums.length == 0 || nums == null) return false;
+        memo = new Index[nums.length];
+        for(int i = 0;i < nums.length;i++) {
+            memo[i] = Index.UNKNOWN;
+        }
+        memo[nums.length - 1] = Index.GOOD;
+        return canJumpHelper(nums, 0); 
+    }
+
+    public boolean canJumpHelper(int[] nums, int nowIndex) {
+        if(memo[nowIndex] != Index.UNKNOWN) {
+            return memo[nowIndex] == Index.GOOD ? true : false;
+        }
+        int furtherestIndex = Math.min(nums[nowIndex] + nowIndex, nums.length - 1);
+        for(int i = furtherestIndex;i >= nowIndex + 1;i--) {
+            if(canJumpHelper(nums, i)) {
+                memo[nowIndex] = Index.GOOD;
+                return true;
+            }
+        }
+        memo[nowIndex] = Index.BAD;
+        return false;
+    }
+}
