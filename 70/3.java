4e290f9f7a06677fc720dbd8789606619a0fa505
GatesMa <1353934265@qq.com>
2020-02-28 21:28:35 +0800

    斐波那契 非递归 空间优化

---
+++ 70/3.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n == 1 || n == 2) return n;
+        int a = 1, b = 2, c = 0;
+        for(int i = 3;i <= n;i ++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
