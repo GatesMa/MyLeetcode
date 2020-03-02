c15952cc8f67bd12f80edccb50cfd1334f025aa2
GatesMa <1353934265@qq.com>
2020-03-02 16:22:55 +0800

    🛋️136 只出现一次的数字 位运算

---
+++ 136/2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = nums[0];
+        if(nums.length > 1) {
+            for(int i = 1;i < nums.length;i++) {
+                ans ^= nums[i];
+            }
+        }
+        return ans;
+    }
+}
